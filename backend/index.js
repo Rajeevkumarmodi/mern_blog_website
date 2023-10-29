8b31321f0dc9b8a3859731a4f301c32039be887f
Rajeevkumarmodi <125949049+Rajeevkumarmodi@users.noreply.github.com>
2023-10-29 12:35:10 +0530

    first server setup

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+app.get("/", (req, res) => {
+  res.end("Hello world");
+});
+
+app.listen(PORT, () => {
+  console.log(`server is start on PORT ${PORT}`);
+});
