349674a377a3c632b096aedb3109f6674eb93d5a
Rajeevkumarmodi <125949049+Rajeevkumarmodi@users.noreply.github.com>
2023-10-29 11:56:16 +0530

    Blog Schema

---
+++ backend/models/Blog.models.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+const { Schema, model } = mongoose;
+
+const blogSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    blogImage: {
+      type: String,
+      required: true,
+    },
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+    likes: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Favourite",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export const Blog = model("Blog", blogSchema);
