ac295abde79368ad52fb8868a5e2ebf097c1e0e5
Rajeevkumarmodi <125949049+Rajeevkumarmodi@users.noreply.github.com>
2023-10-31 12:01:13 +0530

    configure multer

--- backend/controllers/blogControllers.js
+++ backend/controllers/blogControllers.js
@@ -2,5 +2,7 @@ import { Blog } from "../models/Blog.models.js";
 
 export const creatBlogControllers = (req, res) => {
   const { title, description } = req.body;
+  console.log(req.file);
+  console.log(title, description);
   res.send("create blog route");
 };
--- backend/routes/blogRoutes.js
+++ backend/routes/blogRoutes.js
@@ -2,8 +2,9 @@ import express from "express";
 const router = express.Router();
 
 import { creatBlogControllers } from "../controllers/blogControllers.js";
+import upload from "../multerConfig/multerStorage.js";
 
 // creat blog route
-router.post("/createblog", creatBlogControllers);
+router.post("/createblog", upload.single("blogImage"), creatBlogControllers);
 
 export default router;
