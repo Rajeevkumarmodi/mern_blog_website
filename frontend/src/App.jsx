6649f4bd234be1fb2e5eef6fb9d13baaabd6a87e
Rajeevkumarmodi <125949049+Rajeevkumarmodi@users.noreply.github.com>
2023-11-05 11:21:04 +0530

    create blog page setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/header/Header";
 import Signup from "./pages/signup/Signup";
 import Login from "./pages/login/Login";
 import UserHome from "./pages/userHome/UserHome";
+import CreateBlog from "./pages/createBlog/CreateBlog";
 
 function App() {
   return (
@@ -11,9 +12,11 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
+          <Route path="/" element={<UserHome />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/" element={<UserHome />} />
+          <Route path="/creatblog" element={<CreateBlog />} />
+
           <Route path="/profile" element={<p>this is profile page</p>} />
           <Route path="/myblogs" element={<p>this is my blogs page</p>} />
         </Routes>
---
+++ frontend/src/pages/createBlog/CreateBlog.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../../components/layout/Layout";
+
+function CreateBlog() {
+  return (
+    <div>
+      <Layout>this is creat blog app</Layout>
+    </div>
+  );
+}
+
+export default CreateBlog;
